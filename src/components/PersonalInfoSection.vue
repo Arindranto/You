1ae10f59c2be78ae3b6ba829f2c31284b1fa5c66
Arindranto <safidy.herinirina@gmail.com>
2022-05-24 08:18:24 +0300

    Added personal info section into profile page, OnePage style to get started

---
+++ src/components/PersonalInfoSection.vue
@@ -0,0 +1,32 @@
+<!-- The base of a ProfileThing -->
+<template>
+	<div>
+			<h2>{{fullname}}</h2>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			personalInfo: {
+				required: true,
+				default: {
+					firstname: 'Firstname',
+					lastname: 'Lastname',
+					birthdate: 'January 01st, 1970',
+					photo: 'logo.png',
+				}
+			}
+		},
+		data() {
+			const fullname= this.personalInfo.firstname + ' ' + this.personalInfo.lastname.toUpperCase();
+			return {
+				fullname,
+			}
+		},
+	}
+</script>
+
+<style scoped>
+	
+</style>
--- src/components/ProfilePage.vue
+++ src/components/ProfilePage.vue
@@ -1,27 +1,42 @@
 <!-- The base of a ProfileThing -->
 <template>
 	<div>
-			<h2>HOME</h2>
-		<h2>
-			Personnal Informations
-		</h2>
-		<h2>		
-		</h2>
+		<PersonalInfoSection :personalInfo= "personalInfo" />
 	</div>
 </template>
 
 <script>
+	/*
+		components:
+			* PersonalInfoSection
+			* EducationSection
+			* SkillSection
+			* ExperienceSection
+	*/
+	import PersonalInfoSection from '@/components/PersonalInfoSection';
+	import memberList from '@/datas/members-data'
+
 	export default {
+		name: 'ProfilePage',
+		components: {
+			PersonalInfoSection,
+		},
 		props: {
-			personalInfo: {
-				type: Object,
-				required: true,
-				default: {
-					firstname: 'Firstname',
-					lastname: 'Lastname',
-					birthdate: 'January 01st, 1970',
-					photo: 'logo.png',
-				}
+			id: {
+				type: Number,
+				default: 0
+			}
+		},
+		data() {
+			let member= memberList[this.id];	// Get the member data as an object
+			let personalInfo= {
+				firstname: member.personalInfo.firstname,
+				lastname: member.personalInfo.lastname,
+				birthdate: member.personalInfo.birthdate,
+				photo: member.assets.photo
+			}
+			return {
+				personalInfo,
 			}
 		}
 	}
